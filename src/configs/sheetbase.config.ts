9f8521f5779e994595a1ecec6111758eb451f0b2
Lam Nhan <lamnhan92@gmail.com>
2018-09-18 01:48:14 +0700

    update backend url

--- src/configs/sheetbase.config.ts
+++ src/configs/sheetbase.config.ts
@@ -1,6 +1,6 @@
 export const SHEETBASE_CONFIG = {
     // Must be JSON-liked format
     "apiKey": "Qust4st72eFe374ewrechedrUpheStan",
-    "backendUrl": "https://script.google.com/macros/s/AKfycbxfe9TJ2UBnJCgtF5m0yMH04foKedq1QWTh30MG-r6hcXXYOnQ/exec"
+    "backendUrl": "https://script.google.com/macros/s/AKfycbz2S4kJGIiyvjRZ6R12mR15HMt0ddKe0ILxjH5Alqa8qf-PwiS7/exec"
 
 }
